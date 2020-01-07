040dc4faf6d1ba554f28581b4a5ab019761059d6
andrewswint <andrewswint1976@gmail.com>
2020-01-07 11:28:09 -0600

    starting bonuses

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,30 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
+//the url for the page
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+//    page count
+    private int hitCount;
+
+//    page counter
+    public void init() {
+    hitCount = 0;
+}
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        hitCount ++;
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1> <p>Page view counter: " + hitCount + "</p>");
     }
+
 }
